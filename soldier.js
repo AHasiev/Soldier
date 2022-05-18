8166d6790c852fc66f19719804a1a48a8d77c3d4
ahasiev <ahasiev@mail.ru>
2022-05-18 10:52:24 +0300

    Soldier

---
+++ soldier.js
@@ -0,0 +1,20 @@
+const soldier = {
+  name: "Halk",
+  live: 10,
+  gun: {
+      name: "TT",
+      bullet: 30,  
+  }
+  supplies: 3,
+
+  fire: function () {
+
+  }
+  reloadmm: function () {
+
+  }
+
+  hurt: function () {
+
+  }
+}
