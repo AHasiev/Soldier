6d45dbd5f3582abf3d78e4d1d0bcb3ec706745bd
ahasiev <ahasiev@mail.ru>
2022-05-18 11:26:07 +0300

    Soldier two

--- soldier.js
+++ soldier.js
@@ -2,19 +2,27 @@ const soldier = {
   name: "Halk",
   live: 10,
   gun: {
-      name: "TT",
-      bullet: 30,  
-  }
+    name: "TT",
+    bullet: 30,
+  },
   supplies: 3,
 
   fire: function () {
-
-  }
+    this.supplies -= 1;
+    return "бах - бах";
+  },
   reloadmm: function () {
-
-  }
+    this.supplies -= 1;
+    return "Перезарядка...";
+    if (this.supplies === 0) {
+      return "Не осталось припасов";
+    }
+  },
 
   hurt: function () {
-
-  }
-}
+    this.live -= 1;
+    if (this.live === 0) {
+      return "Ты проиграл";
+    }
+  },
+};
